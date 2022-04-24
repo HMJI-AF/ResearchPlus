ab8a3494b8ef90fcb830937d9c73c4540c04e063
Muditha0829 <muditha1gms@gmail.com>
2022-04-24 23:58:40 +0530

    Upgraded upto editable content

--- project-proof/src/component/signup/SignUp.js
+++ project-proof/src/component/signup/SignUp.js
@@ -1,5 +1,6 @@
 import {  Button, Grid, Link, Paper, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom';
 import AppLogo from './../../images/SignIn&SignUp/lwre.png';
 import Logo from './../../images/SignIn&SignUp/sliit_logo.png';
 // import GoogleButton from "react-google-button";
@@ -14,6 +15,11 @@ const bottomText={margin:'10px 0px 10px 0px'};
 // const bottomTextOr={margin:'10px 0px 10px 0px', fontSize:'50px'}
 
 const SignUp=()=>{
+
+  const history = useNavigate();
+    const redirect = (path,data) => {
+      history(path,data)
+    }
 
   const [credentials,setCredentials] = useState({
     email:'',
@@ -58,7 +64,15 @@ const SignUp=()=>{
         <TextField label="Enter SLIIT Email Address" type="email" name="email" fullWidth required style={textStyle} value={credentials.email} onChange={handleChange} />
         <TextField label="Password"  type="password" name="password" fullWidth required style={textStyle} value={credentials.password} onChange={handleChange}/>
         <TextField label="Confirm Password"  type="password" name="cpassword" fullWidth required style={textStyle} value={credentials.cpassword} onChange={handleChange}/>
-        <Button type="submit" color="primary" variant="contained" fullWidth style={btnStyle} >Sign Up</Button>
+        <Button type="submit" color="primary" variant="contained" fullWidth style={btnStyle} disabled={ !(/^([A-Za-z0-9_\-.])+@(["my.sliit"])+\.(["lk"]{2,})$/.test(credentials.email)) }
+        onClick={()=>{
+          if (credentials.password === credentials.cpassword){
+            redirect("/secretsignup",{state:{credentials}});
+          }
+
+          
+
+        }}>Sign Up</Button>
         </form>
         <div align='center' style={bottomText}>
         <Typography>Do you have an account?
