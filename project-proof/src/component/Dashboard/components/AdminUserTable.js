79a819e7419bea470f82059c666ed46f97487004
Muditha0829 <muditha1gms@gmail.com>
2022-05-12 20:24:38 +0530

    Admin Table Fixed

--- project-proof/src/component/Dashboard/components/AdminUserTable.js
+++ project-proof/src/component/Dashboard/components/AdminUserTable.js
@@ -1,11 +1,45 @@
 import { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
 import axios from 'axios';
 
 const columns = [
   { field: '_id', headerName: 'UserID', width: 240 },
   { field: 'email', headerName: 'Email', width: 200 },
   { field: 'role', headerName: 'Role', width: 200 },
+  {
+    headerName:"",
+    field: "Edit",
+    renderCell: (cellValues) => {
+      return (
+        <Button
+          variant="contained"
+          color="primary"
+        //   onClick={(event) => {
+        //     handleClick(event, cellValues);
+        //   }}
+        >
+          Edit
+        </Button>
+      );
+    }
+  },{
+    headerName:"",
+    field: "Delete",
+    renderCell: (cellValues) => {
+      return (
+        <Button
+          variant="contained"
+          color="error"
+        //   onClick={(event) => {
+        //     handleClick(event, cellValues);
+        //   }}
+        >
+          Delete
+        </Button>
+      );
+    }
+  }
 
 ];
 
@@ -24,10 +58,7 @@ function AdminUserTable() {
     setTableData((res.data)))
 
   }, [])
-
-  console.log(tableData)
     
-
 
   return (
       <div>
@@ -40,6 +71,7 @@ function AdminUserTable() {
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
+        customToolbarSelect
       />
     </div></center>
     </div>
